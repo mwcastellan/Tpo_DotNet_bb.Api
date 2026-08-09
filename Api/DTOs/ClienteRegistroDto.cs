d43316ed3a6bb90f82304b4d0cc155e1990c9535
mcastellan1 <101480046+mcastellan1@users.noreply.github.com>
2026-08-09 01:15:17 -0300

    Marcelo W. Castellan

--- Api/DTOs/ClienteRegistroDto.cs
+++ Api/DTOs/ClienteRegistroDto.cs
@@ -1,29 +1,35 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-public class ClienteRegistroDto
+namespace Tpo_DotNet_bb.Api.Api.DTOs
 {
-    [Required]
-    [EmailAddress(ErrorMessage = "EMAIL es incorrecto")]
-    public string Email { get; set; }
+    public class ClienteRegistroDto
+    {
+        [Required(ErrorMessage = "EMAIL es obligatorio")]
+        [EmailAddress(ErrorMessage = "EMAIL es incorrecto")]
+        public string Email { get; set; } = string.Empty;
 
-    [Required]
-    [RegularExpression(@"^[a-zA-Z0-9]+$",
-        ErrorMessage = "APELLIDO es incorrecto")]
-    public string Apellido { get; set; }
+        [Required(ErrorMessage = "APELLIDO es obligatorio")]
+        [RegularExpression(
+            @"^[a-zA-Z0-9]+$",
+            ErrorMessage = "APELLIDO es incorrecto")]
+        public string Apellido { get; set; } = string.Empty;
 
-    [Required]
-    [RegularExpression(@"^[a-zA-Z0-9]+$",
-        ErrorMessage = "NOMBRE es incorrecto")]
-    public string Nombre { get; set; }
+        [Required(ErrorMessage = "NOMBRE es obligatorio")]
+        [RegularExpression(
+            @"^[a-zA-Z0-9]+$",
+            ErrorMessage = "NOMBRE es incorrecto")]
+        public string Nombre { get; set; } = string.Empty;
 
-    [Required]
-    [RegularExpression(@"^[a-zA-Z0-9\s,.'-]{3,}$",
-        ErrorMessage = "DIRECCION es incorrecta")]
-    public string Direccion { get; set; }
+        [Required(ErrorMessage = "DIRECCION es obligatoria")]
+        [RegularExpression(
+            @"^[a-zA-Z0-9\s,.'-]{3,}$",
+            ErrorMessage = "DIRECCION es incorrecta")]
+        public string Direccion { get; set; } = string.Empty;
 
-    [Required]
-    [RegularExpression(
-        @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$",
-        ErrorMessage = "PASSWORD es incorrecto")]
-    public string Password { get; set; }
+        [Required(ErrorMessage = "PASSWORD es obligatorio")]
+        [RegularExpression(
+            @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$",
+            ErrorMessage = "PASSWORD es incorrecto")]
+        public string Password { get; set; } = string.Empty;
+    }
 }
