c842032990839a0cc2c937b82f768aa5f3589ddb
mcastellan1 <101480046+mcastellan1@users.noreply.github.com>
2026-08-09 11:00:04 -0300

    Marcelo W. Castellan

--- Program.cs
+++ Program.cs
@@ -7,74 +7,131 @@ using Tpo_DotNet_bb.Api.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// =====================================================
 // CORS
+// =====================================================
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll", policy =>
+    options.AddPolicy("AllowFront", policy =>
     {
         policy
-            .AllowAnyOrigin()
+            .WithOrigins(
+                "https://tpo-nodejs-bf.vercel.app"
+            )
             .AllowAnyHeader()
-            .AllowAnyMethod();
+            .AllowAnyMethod()
+            .AllowCredentials();
     });
 });
 
-// Base de datos
-var MySql_connect = builder.Configuration["MySql:Connect"]!;
+// =====================================================
+// BASE DE DATOS
+// =====================================================
+var MySql_connect =
+    builder.Configuration["MySql:Connect"]!;
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(
         MySql_connect,
         ServerVersion.Parse("10.11.18-mariadb")
     ));
 
-// Servicios
-builder.Services.AddScoped<ILogProcesoService, LogProcesoService>();
+// =====================================================
+// SERVICIOS
+// =====================================================
+builder.Services.AddScoped
+<
+    ILogProcesoService,
+    LogProcesoService
+>();
 
-// Controllers
+// =====================================================
+// CONTROLLERS
+// =====================================================
 builder.Services.AddControllers();
 
-// Swagger
+// =====================================================
+// SWAGGER
+// =====================================================
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// =====================================================
 // JWT
-var JWT_key = Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!);
+// =====================================================
+var JWT_key = Encoding.UTF8.GetBytes(
+    builder.Configuration["Jwt:Key"]!
+);
 
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
+builder.Services
+    .AddAuthentication(options =>
     {
-        ValidateIssuer = false,
-        ValidateAudience = false,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(JWT_key)
-    };
-});
+        options.DefaultAuthenticateScheme =
+            JwtBearerDefaults.AuthenticationScheme;
+
+        options.DefaultChallengeScheme =
+            JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters =
+            new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+
+                IssuerSigningKey =
+                    new SymmetricSecurityKey(JWT_key),
+
+                ClockSkew = TimeSpan.Zero
+            };
+
+        // ==========================================
+        // LEER JWT DESDE COOKIE
+        // ==========================================
+        options.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
+            {
+                context.Token =
+                    context.Request.Cookies["tpo_dotnet_bb"];
+
+                return Task.CompletedTask;
+            }
+        };
+    });
 
 builder.Services.AddAuthorization();
 
+// =====================================================
+// APP
+// =====================================================
 var app = builder.Build();
 
-// Pipeline
-//mwc if (app.Environment.IsDevelopment())
-// {
+// =====================================================
+// SWAGGER
+// =====================================================
 app.UseSwagger();
 app.UseSwaggerUI();
-//}
 
+// =====================================================
+// PIPELINE
+// =====================================================
 app.UseHttpsRedirection();
-app.UseCors("AllowAll");
+
+app.UseCors("AllowFront");
+
 app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
-// Captura cualquier ruta que no haya sido encontrada
+
+// =====================================================
+// ERROR 404
+// =====================================================
 app.MapFallback(async context =>
 {
     context.Response.StatusCode = 404;
@@ -83,9 +140,9 @@ app.MapFallback(async context =>
     await context.Response.WriteAsJsonAsync(new
     {
         success = false,
-        error = "Endpoint Not Found.",
+        error = "Endpoint Not Found",
         path = context.Request.Path,
-        mensaje = "La ruta solicitada no existe en la API."
+        mensaje = "La ruta solicitada no existe en la API"
     });
 });
 
