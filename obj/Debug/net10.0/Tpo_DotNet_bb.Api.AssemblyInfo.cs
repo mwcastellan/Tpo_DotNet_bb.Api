1003c563838571cff054ebcb0473be6cc092f5c0
mcastellan1 <101480046+mcastellan1@users.noreply.github.com>
2026-08-10 22:33:24 -0300

    Marcelo W. Castellan

--- obj/Debug/net10.0/Tpo_DotNet_bb.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/Tpo_DotNet_bb.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tpo_DotNet_bb.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e257bdd648cde3176953744234020c3e35cec20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8491dda8940c6da481107abdcca88e14cb181ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tpo_DotNet_bb.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tpo_DotNet_bb.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
